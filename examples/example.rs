3c1a4e322b4391c56e50fe847fedb83aee9d6b80
Myse1f <602149025@qq.com>
2023-03-05 17:49:06 +0800

    build endpoint canister

--- examples/example.rs
+++ examples/example.rs
@@ -240,7 +240,11 @@ async fn rpc_call(body: String) -> Result<String, String> {
     Ok(format!("{}", res))
 }
 
+#[cfg(not(any(target_arch = "wasm32", test)))]
 fn main() {
     candid::export_service!();
     std::print!("{}", __export_service());
 }
+
+#[cfg(any(target_arch = "wasm32", test))]
+fn main() {}
