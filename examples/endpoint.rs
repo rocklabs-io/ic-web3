2d72b5d65162e55aa088ca0ad82ac91030e1bc1a
Myse1f <602149025@qq.com>
2023-04-05 20:08:32 +0800

    adjust endpoint canister interface

--- examples/endpoint.rs
+++ examples/endpoint.rs
@@ -1,4 +1,4 @@
-use std::{cell::RefCell};
+use std::{cell::RefCell, collections::{HashSet, HashMap}};
 
 use candid::{CandidType, Deserialize, Principal, candid_method};
 use ic_cdk::api::management_canister::http_request::{TransformArgs, HttpResponse};
@@ -8,26 +8,31 @@ use ic_web3::{transports::ICHttp, Web3};
 // when the both request and response are max 2MB, the const of http calls is about 0.5T
 const MAX_CYCLES_REQUIRES: u128 = 500_000_000_000; // 0.5T
 
-#[derive(CandidType, Deserialize, Clone)]
+#[derive(CandidType, Deserialize)]
 struct State {
     owner: Principal,
-    url: String,
-    api_key: String,
+    controllers: HashSet<Principal>,
+    registered: HashMap<Registered, String>,
+}
+
+#[derive(CandidType, Deserialize, Eq, PartialEq, Hash, Clone)]
+struct Registered {
+    chain_id: u64,
+    api_provider: String,
 }
 
 #[derive(CandidType, Deserialize)]
-struct RpcCallArgs {
-    url: Option<String>,
-    max_response_bytes: Option<u64>,
-    body: String,
+enum RpcTarget {
+    Registered(Registered),
+    UrlWithApiKey(String),
 }
 
 impl Default for State {
     fn default() -> Self {
         Self { 
             owner: Principal::management_canister(), 
-            url: Default::default(), 
-            api_key: Default::default(),
+            controllers: Default::default(),
+            registered: Default::default(),
         }
     }
 }
@@ -38,13 +43,11 @@ thread_local! {
 
 #[init]
 #[candid_method(init)]
-fn init(url: String, api_key: String,) {
+fn init() {
     let owner = ic_cdk::caller();
     STATE.with(|s| {
         let mut state = s.borrow_mut();
         state.owner = owner;
-        state.url = url;
-        state.api_key = api_key;
     });
 }
 
@@ -57,90 +60,68 @@ fn transform(response: TransformArgs) -> HttpResponse {
 }
 
 // get state info
-#[query(name = "getInfo")]
-#[candid_method(query, rename = "getInfo")]
-fn get_info() -> State {
+#[query(name = "registrations")]
+#[candid_method(query, rename = "registrations")]
+fn registrations() -> Vec<Registered> {
     STATE.with(|s| {
         let state = s.borrow();
-        state.clone()
+        state.registered.keys().cloned().collect::<Vec<Registered>>()
     })
 }
 
-// set url
-#[update(name = "setUrl", guard = "is_owner")]
-#[candid_method(update, rename = "setUrl")]
-async fn set_url(url: String) -> bool {
+// add controllers
+#[update(name = "add_controller", guard = "is_owner")]
+#[candid_method(update, rename = "add_controller")]
+async fn add_controller(controller: Principal) {
     STATE.with(|s| {
         let mut state = s.borrow_mut();
-        state.url = url;
+        state.controllers.insert(controller);
     });
-    true
 }
 
-// set api key
-#[update(name = "setAPIKey", guard = "is_owner")]
-#[candid_method(update, rename = "setAPIKey")]
-async fn set_api_key(api_key: String) -> bool {
+// register api and key
+#[update(name = "register_api_key", guard = "is_authorized")]
+#[candid_method(update, rename = "register_api_key")]
+async fn register_api_key(chain_id: u64, api_provider: String, url_with_key: String) {
     STATE.with(|s| {
         let mut state = s.borrow_mut();
-        state.url = api_key;
+        state.registered.insert(Registered{
+            chain_id,
+            api_provider,
+        }, url_with_key);
     });
-    true
 }
 
-/// rpcCallPrivate only owner can call
-/// private call will not charge cycles from caller
-#[update(name = "rpcCallPrivate", guard = "is_owner")]
-#[candid_method(update, rename = "rpcCallPrivate")]
-async fn rpc_call_private(args: RpcCallArgs) -> Result<String, String> {
-    let url_with_key = if let Some(url) = args.url {
-        // if url provided, use the url
-        url
-    } else {
-        // otherwise, use the url and key in this canister
-        let (url, api_key) = STATE.with(|s| {
-            let state = s.borrow();
-            (state.url.clone(), state.api_key.clone())
-        });
-        format!("{}/{}", url.trim_matches(|x| x == '/' || char::is_whitespace(x)), api_key.trim())
-    };
-    
-    let w3 = match ICHttp::new(url_with_key.as_str(), args.max_response_bytes, None) {
-        Ok(v) => { Web3::new(v) },
-        Err(e) => { return Err(e.to_string()) },
-    };
-
-    let res = w3.json_rpc_call(args.body.as_str()).await.map_err(|e| format!("{}", e))?;
-
-    ic_cdk::println!("result: {}", res);
-
-    Ok(format!("{}", res))
-}
-
-/// rpcCall anyone can call, but url must be provided
-/// accept max 0.5T cycles, the rest will be refunded
-#[update(name = "rpcCall")]
-#[candid_method(update, rename = "rpcCall")]
-async fn rpc_call(args: RpcCallArgs) -> Result<String, String> {
+// json rpc call
+#[update(name = "json_rpc")]
+#[candid_method(update, rename = "json_rpc")]
+async fn json_rpc(payload: String, max_response_bytes: Option<u64>, target: RpcTarget) -> Result<String, String> {
     let cycles_call = ic_cdk::api::call::msg_cycles_available128();
     if cycles_call < MAX_CYCLES_REQUIRES {
         return Err(format!("requires {} cycles, get {} cycles", MAX_CYCLES_REQUIRES, cycles_call));
     }
    
-    let url_with_key = if let Some(url) = args.url {
-        // if url provided, use the url
-        url
-    } else {
-        return Err("url must be provided".to_string())
+    let url_with_key = match target {
+        RpcTarget::Registered(registered) => {
+            STATE.with(|s| {
+                s.borrow().registered.get(&registered).cloned().unwrap_or("".to_string())
+            })
+        }
+        RpcTarget::UrlWithApiKey(url_with_api_key) => {
+            url_with_api_key
+        }
+    };
+    if url_with_key.is_empty() {
+        return Err("url is empty".to_string())
     };
     
-    let w3 = match ICHttp::new(url_with_key.as_str(), args.max_response_bytes, None) {
+    let w3 = match ICHttp::new(url_with_key.as_str(), max_response_bytes, None) {
         Ok(v) => { Web3::new(v) },
         Err(e) => { return Err(e.to_string()) },
     };
 
     let cycles_before = ic_cdk::api::canister_balance128();
-    let call_res = w3.json_rpc_call(args.body.as_str()).await;
+    let call_res = w3.json_rpc_call(payload.as_str()).await;
     let cycles_after = ic_cdk::api::canister_balance128();
 
     // add 0.001T cycles as buffer
@@ -154,16 +135,26 @@ async fn rpc_call(args: RpcCallArgs) -> Result<String, String> {
 }
 
 fn is_owner() -> Result<(), String> {
-    let owner = STATE.with(|s| {
+    STATE.with(|s| {
         let state = s.borrow();
-        state.owner
-    });
+        if state.owner == ic_cdk::api::caller() {
+            Ok(())
+        } else {
+            Err("unauthorized".to_string())
+        }
+    })
+}
 
-    if owner == ic_cdk::api::caller() {
-        Ok(())
-    } else {
-        Err("unauthor".to_string())
-    }
+fn is_authorized() -> Result<(), String> {
+    STATE.with(|s| {
+        let state = s.borrow();
+        let caller = ic_cdk::api::caller();
+        if state.owner == caller || state.controllers.contains(&caller){
+            Ok(())
+        } else {
+            Err("unauthorized".to_string())
+        }
+    })
 }
 
 #[pre_upgrade]
